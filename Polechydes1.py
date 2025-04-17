a46d568c3d684473f05c35a7e76dfdb74c0bdfda
Ostrieee <makcvetockovyj@gmail.com>
2025-04-17 23:50:43 +0300

    Create Polechydes1.py

---
+++ Polechydes1.py
@@ -0,0 +1,14 @@
+spisok =[]
+zagadali = random.choice(spisok) 
+inputnie = []
+print('Это игра поле чудес. Начинай отгадать слово') 
+bykvslov = input('Введите букву или слово')
+
+while True:
+    if len(bykvslov) == 1:
+        If bykvslov in zagadali:
+            for i in range zagadali:
+                if i == bykvslov:
+                    print(bykvslov, end='') 
+                else:
+                    print('*', end=''
