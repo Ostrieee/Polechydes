99b0d34c9d12b66f870cbcc0d104b41a40e11143
Ostrieee <makcvetockovyj@gmail.com>
2025-04-18 08:16:43 +0300

    Update Polechydes.py
    Проверки

--- Polechydes.py
+++ Polechydes.py
@@ -3,3 +3,18 @@ zagadali = random.choice(spisok)
 inputnie = []
 print('Это игра поле чудес. Начинай отгадать слово') 
 bykvslov = input('Введите букву или слово')
+while True:
+    if len(bykvslov) == 1:
+        If bykvslov in zagadali:
+            for i in range zagadali:
+                if i == bykvslov:
+                    print(bykvslov, end='') 
+                else:
+                    print('*', end=''
+
+    else:
+        if bykvslov == zagadali:
+            print('Это правильное слово!') 
+        else:
+            print('Это  не правильное слово.')
+    bykvslov = input('Введите букву или слово')
