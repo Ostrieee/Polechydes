5f15342710e3d0975a46125a450e874c115b801a
Ostrieee <makcvetockovyj@gmail.com>
2025-04-18 08:27:15 +0300

    Update Polechydes.py
    изменила проверку буквы, счëт,проверенные

--- Polechydes.py
+++ Polechydes.py
@@ -1,20 +1,25 @@
 spisok =[]
 zagadali = random.choice(spisok) 
 inputnie = []
+zagadalisp = []
+count = 0
+zagadalisp.append(zagadali) 
 print('Это игра поле чудес. Начинай отгадать слово') 
 bykvslov = input('Введите букву или слово')
+
+
 while True:
     if len(bykvslov) == 1:
-        If bykvslov in zagadali:
-            for i in range zagadali:
-                if i == bykvslov:
-                    print(bykvslov, end='') 
-                else:
-                    print('*', end=''
+       for i in range(len(zagadalisp)):
+           if zagadalisp[i] == bykvslov:        
+               zagadalisp[i] = bykvslov:
+
 
     else:
         if bykvslov == zagadali:
-            print('Это правильное слово!') 
+            print('Это правильное слово!')  
         else:
             print('Это  не правильное слово.')
-    bykvslov = input('Введите букву или слово')
+    inputnie.add(bykvslov) 
+    count += 1
+
