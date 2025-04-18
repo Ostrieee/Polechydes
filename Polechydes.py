229a8a69286c86459a918953ba38961eddff7c18
Ostrieee <makcvetockovyj@gmail.com>
2025-04-18 08:38:45 +0300

    Update Polechydes.py

--- Polechydes.py
+++ Polechydes.py
@@ -1,26 +1,32 @@
 spisok =[]
 zagadali = random.choice(spisok) 
 inputnie = []
-zagadalisp = []
+nadosp = []
+nadosp.append(zagadali) 
+zagadalisp = [*] * len(zagadali) 
 count = 0
-zagadalisp.append(zagadali) 
 print('Это игра поле чудес. Начинай отгадать слово') 
 bykvslov = input('Введите букву или слово')
 
 
 while True:
-    if len(bykvslov) == 1:
-       for i in range(len(zagadali)):
-           if zagadali[i] == bykvslov:        
-               zagadalisp[i] = bykvslov:
-       for j in zagadalisp:
-           print(j, end='') 
+    if bykvslov not in inputnie:
+        if len(bykvslov) == 1:
+            if zagadalisp != nadosp:
+               for i in range(len(zagadali)):
+                  if zagadali[i] == bykvslov:        
+                     zagadalisp[i] = bykvslov:
+            for j in zagadalisp:
+            print(j, end='') 
 
-    else:
-        if bykvslov == zagadali:
-            print('Это правильное слово!')  
         else:
-            print('Это  не правильное слово.')
-    inputnie.add(bykvslov) 
-    count += 1
+            if bykvslov == zagadali:
+                print('Это правильное слово!')
+                break
+            else:
+                print('Это  не правильное слово.')
+        inputnie.add(bykvslov) 
+        count += 1
+    else:
+        print('Это уже было введено') 
 
